7e2080f8534e85c7dc2864203fbf65b5fdb1503d
Antek Ostrowski <antek.ostrowski@zs1mm.local>
2024-12-06 16:49:29 +0100

    poprawka moj profil

--- next-app/src/app/moj-profil/data-acces.js
+++ next-app/src/app/moj-profil/data-acces.js
@@ -1,25 +1,29 @@
 import { pocketbase } from "@/lib/pocketbase";
 
-export const fetchZBiorkiInUzytkownik = async (idUser) => {
-    return await pocketbase.collection('Zbiorki').getFullList({
-        filter: `id_autora = ${idUser}`,
+export async function getWplaty() {
+  try {
+    const records = await pocketbase.collection("wplaty").getFullList({
+      sort: "data_utworzenia",
     });
-  };
+    return records;
+  } catch (error) {
+    throw new Error(error);
+  }
+}
+export async function getTitleFromWplata(idZbiorki) {
+  console.log(idZbiorki);
 
-  export const fetchKomentarzeInUzytkownik = async (idUser) => {
-    return await pocketbase.collection('komentarze').getFullList({
-        filter: `id_autora = ${idUser}`,
+  try {
+    const records = await pocketbase.collection("Zbiorki").getFullList({
+      filter: pocketbase.filter(`id ~ {:idZbiorki}`, {
+        idZbiorki: idZbiorki,
+      }),
     });
-  };
 
-  export const fetchProblemyInUzytkownik = async (idUser) => {
-    return await pocketbase.collection('problemy').getFullList({
-        filter: `id_autora = ${idUser}`,
-    });
-  };
+    // console.log(records);
 
-  export const fetchWplatyInUzytkownik = async (idUser) => {
-    return await pocketbase.collection('wplaty').getFullList({
-        filter: `id_autora = ${idUser}`,
-    });
-  };
+    return records;
+  } catch (error) {
+    throw new Error(error);
+  }
+}
--- next-app/src/app/moj-profil/page.js
+++ next-app/src/app/moj-profil/page.js
@@ -1,7 +1,7 @@
-"use client"
+"use client";
 
-import { useUser } from "@/hooks/useUser"
-import { Button } from "@/components/ui/button"
+import { useUser } from "@/hooks/useUser";
+import { Button } from "@/components/ui/button";
 import {
   Dialog,
   DialogContent,
@@ -9,115 +9,71 @@ import {
   DialogHeader,
   DialogTitle,
   DialogTrigger,
-} from "@/components/ui/dialog"
-import {
-  Card,
-  CardContent,
-  CardFooter,
-  CardHeader,
-  CardTitle,
-} from "@/components/ui/card"
-import { Input } from "@/components/ui/input"
-import { Label } from "@/components/ui/label"
-import { useQuery } from "@tanstack/react-query"
-import { fetchKomentarzeInUzytkownik, fetchProblemyInUzytkownik, fetchUzytkownicy, fetchWplatyInUzytkownik, fetchZBiorkiInUzytkownik } from "./data-acces"
+} from "@/components/ui/dialog";
 
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+import Wplata from "./Wplata";
+import { useQuery } from "@tanstack/react-query";
+import { getWplaty } from "./data-acces";
 
-export default function Page(){
-
- const {user} = useUser()
-  console.log("user",user)
-
-  const { zbiorki } = useQuery({
-    queryKey: ['getZbiorkiUserInfo'],
-    queryFn: fetchZBiorkiInUzytkownik(user?.id),
+export default function Page() {
+  const {
+    data: wplaty,
+    isLoading,
+    error,
+  } = useQuery({
+    queryKey: ["listaWplat"],
+    queryFn: () => getWplaty(),
   });
 
-  const { komentarze } = useQuery({
-    queryKey: ['getZbiorkiUserInfo'],
-    queryFn: fetchKomentarzeInUzytkownik(user?.id),
-  });
-
-  const { problemy } = useQuery({
-    queryKey: ['getProblemyUserInfo'],
-    queryFn: fetchProblemyInUzytkownik(user?.id),
-  });
-
-  const { wplaty } = useQuery({
-    queryKey: ['getWplatyUserInfo'],
-    queryFn: fetchWplatyInUzytkownik(user?.id),
-  });
-
-   
-    return(
-        <div className="flex flex-col items-center justify-center m-4">
-            <h1>Zmień szczegóły konta</h1>
-            <div className="">
-                <p>Imię: {}</p>
-                <p>Nazwisko: {}</p>
-                <div className="">
-                <Dialog>
-      <DialogTrigger asChild>
-        <Button variant="outline">Edit Profile</Button>
-      </DialogTrigger>
-      <DialogContent className="sm:max-w-[425px]">
-        <DialogHeader>
-          <DialogTitle>Edytuj dane </DialogTitle>
-        </DialogHeader>
-        <div className="grid gap-4 py-4">
-          <div className="grid grid-cols-4 items-center gap-4">
-            <Label htmlFor="name" className="text-right">
-              Imię
-            </Label>
-            <Input
-              id="name"
-              defaultValue="Pobrac z bazy"
-              className="col-span-3"
-            />
-          </div>
-          <div className="grid grid-cols-4 items-center gap-4">
-            <Label htmlFor="username" className="text-right">
-              Nazwisko
-            </Label>
-            <Input
-              id="username"
-              defaultValue="pobrac z bazy"
-              className="col-span-3"
-            />
-          </div>
-        </div>
-        <DialogFooter>
-          <Button type="submit">Zapisz</Button>
-        </DialogFooter>
-      </DialogContent>
-    </Dialog>
+  return (
+    <div className="flex flex-col items-center justify-center m-4">
+      <h1>Zmień szczegóły konta</h1>
+      <div className="">
+        <p>Imię: {}</p>
+        <p>Nazwisko: {}</p>
+        <div className="">
+          <Dialog>
+            <DialogTrigger asChild>
+              <Button variant="outline">Edit Profile</Button>
+            </DialogTrigger>
+            <DialogContent className="sm:max-w-[425px]">
+              <DialogHeader>
+                <DialogTitle>Edytuj dane </DialogTitle>
+              </DialogHeader>
+              <div className="grid gap-4 py-4">
+                <div className="grid grid-cols-4 items-center gap-4">
+                  <Label htmlFor="name" className="text-right">
+                    Imię
+                  </Label>
+                  <Input
+                    id="name"
+                    defaultValue="Pobrac z bazy"
+                    className="col-span-3"
+                  />
                 </div>
-            </div>
-            <div>
-                <h1>Historia wpłat</h1>
-                <Card className="w-[350px]">
-      <CardHeader>
-        <CardTitle>*tytul zbiorki*</CardTitle>
-      </CardHeader>
-      <CardContent>
-        <form>
-          <div className="grid w-full items-center gap-4">
-          <div className="flex flex-col space-y-1.5">
-              <Label htmlFor="name">kiedy wplacono:</Label>
-            </div>
-            <div className="flex flex-col space-y-1.5">
-              <Label htmlFor="name">ile wpłacono:</Label>
-            </div>
-            <div className="flex flex-col space-y-1.5">
-              <Label></Label>
-            </div>
-          </div>
-        </form>
-      </CardContent>
-      <CardFooter className="flex justify-between">
-      </CardFooter>
-    </Card>
-            </div>
+                <div className="grid grid-cols-4 items-center gap-4">
+                  <Label htmlFor="username" className="text-right">
+                    Nazwisko
+                  </Label>
+                  <Input
+                    id="username"
+                    defaultValue="pobrac z bazy"
+                    className="col-span-3"
+                  />
+                </div>
+              </div>
+              <DialogFooter>
+                <Button type="submit">Zapisz</Button>
+              </DialogFooter>
+            </DialogContent>
+          </Dialog>
         </div>
-    )
+      </div>
+      {wplaty?.map((wplata) => {
+        return <Wplata key={wplata.id} wplata={wplata} />;
+      })}
+    </div>
+  );
 }
